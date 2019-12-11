09bdb637b5d12f322051eaf0a266250dbeedc2ee
LiXianXu <857251389@qq.com>
2019-12-11 09:44:38 +0800

    first commit

---
+++ src/main/java/lxx/ligenote/LigenoteApplication.java
@@ -0,0 +1,13 @@
+package lxx.ligenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigenoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LigenoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lxx/ligenote/LigenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package lxx.ligenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigenoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
